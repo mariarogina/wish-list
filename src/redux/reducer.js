4926ad23e1a7cdfc9be5f518bd8b69fdebf60bb9
Mariia <whitesheepa@gmail.com>
2021-11-03 07:28:16 +0200

    reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,4 @@
 import { combineReducers } from "redux";
 import wishListReducer, { moduleName as wishListModule } from "./wishList";
 
-// export default combineReducers({ [wishListModule]: wishListReducer });
-
-export default combineReducers({ wishListReducer: wishListReducer });
+export default combineReducers({ [wishListModule]: wishListReducer });
